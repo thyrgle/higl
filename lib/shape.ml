bbc75a554a22cee3bb6664ed086646e0c932025e
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-08-29 11:16:33 -0700

    Fixed compiler errors, still does not work.

--- lib/shape.ml
+++ lib/shape.ml
@@ -27,14 +27,16 @@ type geom_manager =
   shapes: primitive list;
 }
 
-(**
+let draw_primitive (shape: primitive) = ()
+
+
 let draw (shapes: primitive list) =
+  List.iter draw_primitive shapes
 
 let to_screen_buffer (manager: geom_manager) =
   Gl.clear_color 0. 0. 0. 1.;
   Gl.clear Gl.color_buffer_bit;
-  Gl.use_program pid;
-  Gl.bnd_vertex_array gid;
+  Gl.use_program 0;
+  Gl.bind_vertex_array 0;
   draw manager.shapes;
   Gl.bind_vertex_array 0
-*)
